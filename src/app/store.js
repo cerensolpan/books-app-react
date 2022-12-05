a2bcf8e369e0e5e09f4ed94d01ae060e268d0355
cerensolpan <cerensolpan@gmail.com>
2022-12-05 23:58:35 +0300

    Add provider and store to index.js

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from "../features/booksSlice"
+
+export const store = configureStore({
+  reducer: {
+    books: booksReducer
+  },
+})
