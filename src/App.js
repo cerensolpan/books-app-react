6d880b60f549fe39b7ec228439613ced8e4d04d7
cerensolpan <cerensolpan@gmail.com>
2022-12-06 23:53:18 +0300

    Get search result to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import { useSelector } from "react-redux";
+import { books } from "./features/booksSlice";
 
 function App() {
+  const booksResult = useSelector(books);
+
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <div className="p-4">
+        {booksResult.length > 0 && (
+          <div className="space-y-2">
+            {booksResult.map((book) => (
+              <Card book={book} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
