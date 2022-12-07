1d0ef32ea2a6d50545be4c6c4a5c46c15db7ae28
cerensolpan <cerensolpan@gmail.com>
2022-12-08 00:29:26 +0300

    Add Loading Circle and alert to App.js and get books

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import "./App.css";
-import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 import { useSelector } from "react-redux";
-import { books } from "./features/booksSlice";
+import { booksSelector } from "./features/booksSlice";
+import LoadingCircle from "./components/LoadingCircle";
+import Alert from "./components/Alert";
 
 function App() {
-  const booksResult = useSelector(books);
+  const { books, status } = useSelector(booksSelector);
 
   return (
     <div className="App">
       <Navbar />
-      <div className="p-4">
-        {booksResult.length > 0 && (
-          <div className="space-y-2">
-            {booksResult.map((book) => (
-              <Card book={book} />
-            ))}
+      <div className="p-4 bg-gray-800 h-screen">
+        {status === "loading" ? (
+          <LoadingCircle />
+        ) : books !== undefined ? (
+          <>
+            {books.length > 0 && (
+              <div className="space-y-2">
+                {books.map((book, index) => (
+                  <Card book={book} key={index} />
+                ))}
+              </div>
+            )}
+          </>
+        ) : (
+          <div>
+            <Alert />
           </div>
         )}
       </div>
