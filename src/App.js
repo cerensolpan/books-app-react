770bba29213cd85dff5eb231962714f3f794327f
cerensolpan <cerensolpan@gmail.com>
2022-12-10 23:41:02 +0300

    Add router and route to app.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Card from "./components/Card";
-import { useSelector } from "react-redux";
-import { booksSelector } from "./features/booksSlice";
-import LoadingCircle from "./components/LoadingCircle";
-import Alert from "./components/Alert";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Book from "./components/Book";
+import Books from "./components/Books";
 
 function App() {
-  const { books, status } = useSelector(booksSelector);
+ 
 
   return (
     <div className="App">
+      <Router>
       <Navbar />
-      <div className="p-4 bg-gray-800 h-screen">
-        {status === "loading" ? (
-          <LoadingCircle />
-        ) : books !== undefined ? (
-          <>
-            {books.length > 0 && (
-              <div className="space-y-2">
-                {books.map((book, index) => (
-                  <Card book={book} key={index} />
-                ))}
-              </div>
-            )}
-          </>
-        ) : (
-          <div>
-            <Alert />
-          </div>
-        )}
-      </div>
+      <Routes>
+          <Route exact path="/" element={<Books />}></Route>
+          <Route exact path="/book/:id" element={<Book />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
