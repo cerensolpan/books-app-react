6a2cc7e5ca26cc06a7fd305686b644da08c224b1
cerensolpan <cerensolpan@gmail.com>
2022-12-10 23:47:33 +0300

    Create dynamic the book's detail page

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,26 @@
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
 
-export default function Book(){
-    const location= useLocation();
-    const book = location.state.book
+export default function Book() {
+  const location = useLocation();
+  const book = location.state.book;
 
-    return(
-        <div>book</div>
-    )
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="img-book" />
+      <div className="flex flex-col text-start space-y-2">
+        <span className="text-violet-500 text-xl">{book.volumeInfo.title}</span>
+        <div className="flex space-x-1">
+          {book.volumeInfo?.authors?.map((author) => (
+            <span className="text-violet-500 text-sm">{author}</span>
+          ))}
+          <span className="text-violet-200 text-sm">
+            · {book.volumeInfo?.publishedDate?.split("-", 1)}
+          </span>
+        </div>
+        <span className="text-violet-200 text-sm line-clamp-3">
+          {book.volumeInfo.description}
+        </span>
+      </div>
+    </div>
+  );
 }
