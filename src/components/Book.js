4d4d60d339bf2eebfabdba4e773bc6b78a0f6fdf
cerensolpan <cerensolpan@gmail.com>
2022-12-11 23:53:19 +0300

    Change style and add average rating on book detail page

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,25 +1,55 @@
 import { useLocation } from "react-router-dom";
+import StarIcon from "./StarIcon";
 
 export default function Book() {
   const location = useLocation();
   const book = location.state.book;
-
+  console.log("book", book);
   return (
-    <div className="flex flex-col justify-center items-center">
-      <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="img-book" />
-      <div className="flex flex-col text-start space-y-2">
-        <span className="text-violet-500 text-xl">{book.volumeInfo.title}</span>
-        <div className="flex space-x-1">
-          {book.volumeInfo?.authors?.map((author) => (
-            <span className="text-violet-500 text-sm">{author}</span>
-          ))}
-          <span className="text-violet-200 text-sm">
-            · {book.volumeInfo?.publishedDate?.split("-", 1)}
+    <div className="p-4 bg-gray-800 h-screen px-32">
+      <div className="flex flex-col justify-center items-center">
+        <img
+          src={book.volumeInfo.imageLinks.thumbnail}
+          alt="img-book"
+          width={200}
+        />
+        <div className="flex flex-col items-center text-start space-y-2">
+          <span className="text-violet-500 text-xl mt-6">
+            {book.volumeInfo.title}
+          </span>
+
+          <div className="text-start space-y-2">
+            {book.volumeInfo?.authors?.map((author) => (
+              <div className="text-sm">
+                <span className="text-violet-200">Author: </span>
+                <span className="text-violet-500">{author}</span>
+              </div>
+            ))}
+            {book.volumeInfo?.publishedDate && (
+              <div className="text-sm">
+                <span className="text-violet-200">Published Date: </span>
+                <span className="text-violet-500">
+                  {book.volumeInfo?.publishedDate?.split("-", 1)}
+                </span>
+              </div>
+            )}
+            {book.volumeInfo?.averageRating && (
+              <div>
+                <div className="flex items-center space-x-2 text-sm">
+                  <span className="text-violet-200">Average Rating: </span>
+
+                  <StarIcon fill="#FFFF00" />
+                  <span className="text-violet-500">
+                    {book.volumeInfo?.averageRating}
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
+          <span className="text-violet-200 pt-4 text-justify">
+            {book.volumeInfo.description}
           </span>
         </div>
-        <span className="text-violet-200 text-sm line-clamp-3">
-          {book.volumeInfo.description}
-        </span>
       </div>
     </div>
   );
