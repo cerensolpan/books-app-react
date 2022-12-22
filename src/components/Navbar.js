04edf3ba07ac94847602f5f142634aedc96266f5
cerensolpan <cerensolpan@gmail.com>
2022-12-22 22:52:13 +0300

    Delete unnecessary link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,12 +32,6 @@ export default function Navbar() {
                     <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">
                       Home
                     </a>
-                    <a
-                      href="#"
-                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                    >
-                      Books
-                    </a>
                   </div>
                 </div>
               </div>
