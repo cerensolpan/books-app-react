acffc5e02a7eeec709d04c0f7edf207c28471d7a
cerensolpan <cerensolpan@gmail.com>
2022-12-23 23:36:45 +0300

    Add font family to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ubuntu: ['"Ubuntu",sans-serif', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
